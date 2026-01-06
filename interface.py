3096f8a4d787559a37c9207a26d341815b1f3381
Alán F. Muñoz <afer.mg@gmail.com>
2026-01-05 21:52:05 -0500

    feat: enable WASM support and add Zenodo upload automation
    Refactor `interface.py` to use `pyfetch` and remote data storage for
    browser compatibility. Add `upload_zenodo.sh` and `metadata.json` to
    automate data publishing and update the documentation accordingly.

--- interface.py
+++ interface.py
@@ -1,31 +1,30 @@
 import marimo
 
-__generated_with = "0.11.23"
-app = marimo.App(width="medium")
+__generated_with = "0.11.31-dev3"
+app = marimo.App()
 
 
 @app.cell
 def _():
-    import json
-
-    import duckdb
     import marimo as mo
-    import requests
+    import duckdb
+    import pandas as pd
+    import pyarrow
+    from pyodide.http import pyfetch
+    return duckdb, mo, pd, pyarrow, pyfetch
 
-    def get_pubchem_id(common_name, identifier):
-        url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/{identifier}/{common_name}/json"
-        response = requests.get(url)
 
-        return response
-    return duckdb, get_pubchem_id, json, mo, requests
+@app.cell
+def _(pd):
+    pq = pd.read_parquet('https://zenodo.org/api/records/15104315/files/jump_pubchem.parquet/content')
+    return (pq,)
 
 
 @app.cell
 def _(mo):
-    data_path = str(mo.notebook_location() / "data" / "jump_pubchem.parquet")
     mapper = mo.sql(
         f"""
-        SELECT *,(((from_hex(fingerprint)::BITSTRING)::VARCHAR)[33:-8]) AS bs FROM read_parquet('{data_path}');
+        SELECT *,(((from_hex(fingerprint)::BITSTRING)::VARCHAR)[33:-8]) AS bs FROM pq;
         """,
         output=False
     )
@@ -65,35 +64,26 @@ def _(field, mo, sb):
 
 
 @app.cell
-def _():
-    return
-
-
-@app.cell
-def _(duckdb, field, get_pubchem_id, mapper, mo, res, sb):
+async def _(field, mo, pyfetch, sb):
     if sb.value:
         sim_col = "similarity"
-        result = get_pubchem_id(sb.value, field.value)
-        fmt = result.json()
+        # result = get_pubchem_id(sb.value, field.value)
+        result = await pyfetch(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/{field.value}/{sb.value}/json")
+        fmt = await result.json()
         if "Fault" in fmt:
-            mo.md("ID not found")
+            output = mo.md("Error: ID not found")
         else:
-        #assert "fault" not in fmt, "No CID Found"
             options = [{x["urn"]["label"]:list(x["value"].values())[0] for x in hit["props"] if (x["urn"]["label"]=="InChIKey") or (x["urn"]["label"] == "IUPAC Name" and x["urn"]["name"]=="Preferred") or (x["urn"]["label"]=="Fingerprint") } for hit in fmt["PC_Compounds"]]
             # Convert to bitstring
             fp_hex = options[0]["Fingerprint"]
             bitstring = (bin(int(fp_hex, 16))).zfill(len(fp_hex)*4)[32:-7]
 
             # Query against JUMP->PubChem mapper
-            res = duckdb.sql(
+            output = mo.sql(
                 f"SELECT Metadata_JCP2022,InChIKey,BIT_COUNT(bs::BITSTRING & '{bitstring}') / BIT_COUNT(bs::BITSTRING | '{bitstring}') AS {sim_col} FROM mapper ORDER BY {sim_col} DESC LIMIT 3"
             )
-            mo.sql(
-            f"""
-            SELECT * FROM res
-            """
-        )
-    return bitstring, fmt, fp_hex, options, res, result, sim_col
+        output
+    return bitstring, fmt, fp_hex, options, output, result, sim_col
 
 
 if __name__ == "__main__":
---
+++ upload_zenodo.sh
@@ -0,0 +1,67 @@
+# Find the latest version of the dataset
+ZENODO_ENDPOINT="https://zenodo.org"
+DEPOSITION_PREFIX="${ZENODO_ENDPOINT}/api/deposit/depositions"
+ORIGINAL_ID="15644946" # jump_rr tables
+META_FILE="metadata.json"
+# ORIGINAL_ID="15644588" # database mapper table
+# META_FILE="metadata/jcp_to_dbs.json"
+DIR_TO_VERSION="$1"
+
+if [ -z "${ORIGINAL_ID}" ]; then # Only get latest id when provided an original one
+	echo "Creating new deposition"
+	DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}"
+else # Update existing dataset
+	echo "Creating new version"
+	LATEST_ID=$(curl "${ZENODO_ENDPOINT}/records/${ORIGINAL_ID}/latest" |
+		grep records | sed 's/.*href=".*\.org\/records\/\(.*\)".*/\1/')
+	DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}/${LATEST_ID}/actions/newversion"
+fi
+
+if [ -z "${ZENODO_TOKEN}" ]; then # Check Zenodo Token
+	echo "Access token not available"
+	exit 1
+else
+	echo "Access token found."
+fi
+
+# Create new deposition
+DEPOSITION=$(curl -H "Content-Type: application/json" \
+	-X POST --data "{}" \
+	"${DEPOSITION_ENDPOINT}?access_token=${ZENODO_TOKEN}" |
+	jq .id)
+echo "New deposition ID is ${DEPOSITION}"
+
+# Variables
+curl "${DEPOSITION_PREFIX}?access_token=${ZENODO_TOKEN}"
+BUCKET_DATA=$(curl "${DEPOSITION_PREFIX}/${DEPOSITION}?access_token=${ZENODO_TOKEN}")
+BUCKET=$(echo "${BUCKET_DATA}" | jq --raw-output .links.bucket)
+
+if [ "${BUCKET}" = "null" ]; then
+	echo "Could not find URL for upload. Response from server:"
+	echo "${BUCKET_DATA}"
+	exit 1
+fi
+
+# Upload file
+echo "Uploading files to bucket ${BUCKET}"
+for FILE_TO_VERSION in $(find "${DIR_TO_VERSION}" -name '*.parquet'); do
+	echo "${FILE_TO_VERSION}"
+	curl --retry 5 \
+		--retry-delay 5 \
+		-o /dev/null \
+		--upload-file ${FILE_TO_VERSION} \
+		"${BUCKET}/${FILE_TO_VERSION##*/}?access_token=${ZENODO_TOKEN}"
+done
+
+NEW_DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}/${DEPOSITION}"
+echo "Uploading file to ${NEW_DEPOSITION_ENDPOINT}"
+curl -H "Content-Type: application/json" \
+	-X PUT --data @${META_FILE} \
+	"${NEW_DEPOSITION_ENDPOINT}?access_token=${ZENODO_TOKEN}"
+
+# Publish
+# echo "Publishing to ${NEW_DEPOSITION_ENDPOINT}"
+curl -H "Content-Type: application/json" \
+	-X POST --data "{}" \
+	"${NEW_DEPOSITION_ENDPOINT}/actions/publish?access_token=${ZENODO_TOKEN}" |
+	jq .id
